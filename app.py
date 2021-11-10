939b6434f7da67c98fd8a2fb7f2ce0616694f880
Gbadamosi_oluwaseyi <48827686+seyirex@users.noreply.github.com>
2021-11-10 21:05:48 +0100

    update

--- app.py
+++ app.py
@@ -18,7 +18,7 @@ menu_items = {
 st.set_page_config(page_title="Terminator", page_icon="./images/pest-control.png", layout='centered',menu_items=menu_items)
 st.set_option('deprecation.showfileUploaderEncoding', False)
 
-@st.cache(allow_output_mutation=True)
+@st.cache(hash_funcs={FooType: hash})
 def load_model():
 	model = tf.keras.models.load_model('./model/keras_model.h5')
 	return model
